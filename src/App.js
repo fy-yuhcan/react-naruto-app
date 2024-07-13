7677f3b4fac508eb576a20b7f4b9b3946a1c2dea
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-13 14:50:29 +0900

    カードの詳細を取得

--- src/App.js
+++ src/App.js
@@ -21,6 +21,17 @@ function App() {
               <img src={character.images[0] != null ? character.images[0]: "dummy.png"}
               alt="character" 
               className='card-image' />
+              <div className='card-content'>
+                  <h3 className='card-title'>{character.name}</h3>
+                  <p className='card-description'>
+                    {character.debut?.appearsIn ?? 'なし'}
+                  </p>
+                  <div className='card-footer'>
+                    <span className='affiliation'>
+                      {character.personal?.affiliation ?? 'なし'}
+                    </span>
+                  </div>
+              </div>
             </div>)
           })}
         </div>
